18757ea42485cb4c694d0701198d0111e3e1b894
Dan Barber <github@danbarber.me>
2016-12-08 12:16:11 +0000

    Plumb in redux

--- app/reducers/chess-board.js
+++ app/reducers/chess-board.js
@@ -1,9 +1,19 @@
-import { defaultState } from "store/default_state";
+import defaultState from "store/default-state";
 
-export const chessBoard = (state = defaultState, action) => {
+const chessBoardReducer = (state = defaultState, action) => {
   switch (action.type) {
+    case "MOVE_PIECE":
+      var piece = state.board[from.rank][from.file];
+      state.board[to.rank][to.file] = piece;
+      state.board[from.rank][from.file] = null;
+      return state;
+
+    case "SELECT_PIECE":
+      return Object.assign({}, state, { selectedSquare: action.coords });
+
     default:
       return state;
   }
 }
 
+export default chessBoardReducer;
