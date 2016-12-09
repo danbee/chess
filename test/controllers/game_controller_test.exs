b479f0eadeb491b71d41b4ae18916e546413a521
Dan Barber <github@danbarber.me>
2016-12-09 14:52:46 +0000

    Remove unnecessary test

--- test/controllers/game_controller_test.exs
+++ test/controllers/game_controller_test.exs
@@ -10,11 +10,6 @@ defmodule Chess.GameControllerTest do
     assert html_response(conn, 200) =~ "Listing games"
   end
 
-  test "renders form for new resources", %{conn: conn} do
-    conn = get conn, game_path(conn, :new)
-    assert html_response(conn, 200) =~ "New game"
-  end
-
   test "creates resource and redirects when data is valid", %{conn: conn} do
     conn = post conn, game_path(conn, :create), game: @valid_attrs
     assert redirected_to(conn) == game_path(conn, :index)
