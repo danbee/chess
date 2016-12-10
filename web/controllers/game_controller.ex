6f7a6e128836a44c48c98a75ced78299d36ed83b
Dan Barber <github@danbarber.me>
2016-12-10 14:03:19 +0000

    Tidy the fuck up a little

--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -27,26 +27,6 @@ defmodule Chess.GameController do
     render(conn, "show.html", game: game)
   end
 
-  def edit(conn, %{"id" => id}) do
-    game = Repo.get!(Game, id)
-    changeset = Game.changeset(game)
-    render(conn, "edit.html", game: game, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "game" => game_params}) do
-    game = Repo.get!(Game, id)
-    changeset = Game.changeset(game, game_params)
-
-    case Repo.update(changeset) do
-      {:ok, game} ->
-        conn
-        |> put_flash(:info, "Game updated successfully.")
-        |> redirect(to: game_path(conn, :show, game))
-      {:error, changeset} ->
-        render(conn, "edit.html", game: game, changeset: changeset)
-    end
-  end
-
   def delete(conn, %{"id" => id}) do
     game = Repo.get!(Game, id)
 
