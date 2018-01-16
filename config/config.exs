9be77a9ad066d4fbe65ee9f659bbcaf141091e1f
Dan Barber <github@danbarber.me>
2018-01-15 22:24:41 -0500

    Add Guardian configuration

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,11 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Configure authentication provider
+config :chess, Chess.Auth.Guardian,
+  issuer: "chess",
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
