3f3943ee5db68b3b5da154949a3c6ce29f54e33b
Dan Barber <github@danbarber.me>
2018-02-21 21:20:37 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -2,26 +2,42 @@ defmodule Chess.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :chess,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :chess,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      deps: deps(),
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Chess, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger,
-                    :gettext, :phoenix_ecto, :postgrex, :timex_ecto,
-                    :argon2_elixir, :comeonin, :formulator, :guardian,
-                    :elixir_make]]
+    [
+      mod: {Chess, []},
+      applications: [
+        :argon2_elixir,
+        :comeonin,
+        :cowboy,
+        :elixir_make,
+        :formulator,
+        :gettext,
+        :guardian,
+        :logger,
+        :phoenix,
+        :phoenix_ecto,
+        :phoenix_html,
+        :phoenix_pubsub,
+        :postgrex,
+        :timex_ecto,
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -32,22 +48,24 @@ defmodule Chess.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:argon2_elixir, "~> 1.2"},
-     {:comeonin, "~> 4.0"},
-     {:credo, "~> 0.8", only: [:dev, :test]},
-     {:distillery, "~> 1.5", runtime: false},
-     {:formulator, "~> 0.1.6"},
-     {:phoenix, "~> 1.3.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.0"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.15.0"},
-     {:guardian, "~> 1.0"},
-     {:cowboy, "~> 1.0"},
-     {:timex_ecto, "~> 3.0"},
-     {:wallaby, "~> 0.19.2", [runtime: false, only: :test]}]
+    [
+      {:argon2_elixir, "~> 1.2"},
+      {:comeonin, "~> 4.0"},
+      {:cowboy, "~> 1.0"},
+      {:credo, "~> 0.8", only: [:dev, :test]},
+      {:distillery, "~> 1.5", runtime: false},
+      {:formulator, "~> 0.1.6"},
+      {:gettext, "~> 0.15.0"},
+      {:guardian, "~> 1.0"},
+      {:phoenix, "~> 1.3.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_html, "~> 2.0"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:timex_ecto, "~> 3.2"},
+      {:wallaby, "~> 0.19.2", [runtime: false, only: :test]},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -57,8 +75,10 @@ defmodule Chess.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+    ]
   end
 end
