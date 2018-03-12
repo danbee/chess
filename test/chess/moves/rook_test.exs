af8027e6d84aed010ec95a05ce7a194909ec4b62
Dan Barber <github@danbarber.me>
2018-03-12 14:18:13 -0400

    💄

--- test/chess/moves/rook_test.exs
+++ test/chess/moves/rook_test.exs
@@ -9,7 +9,7 @@ defmodule Chess.Moves.RookTest do
 
     expected_moves = Enum.sort([
       {4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 6}, {4, 7},
-      {0, 5}, {1, 5}, {2, 5}, {3, 5}, {5, 5}, {6, 5}, {7, 5}, 
+      {0, 5}, {1, 5}, {2, 5}, {3, 5}, {5, 5}, {6, 5}, {7, 5},
     ])
     assert Enum.sort(moves) == expected_moves
   end
@@ -20,7 +20,7 @@ defmodule Chess.Moves.RookTest do
 
     expected_moves = Enum.sort([
       {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
-      {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, 
+      {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0},
     ])
     assert Enum.sort(moves) == expected_moves
   end
