8efecebc4a9e56a7422ebf6610f00728829b1a5b
Dan Barber <github@danbarber.me>
2018-03-12 13:49:29 -0400

    Stop all other pieces causing an error

--- lib/chess/moves.ex
+++ lib/chess/moves.ex
@@ -12,6 +12,14 @@ defmodule Chess.Moves do
         Pawn.moves(board, {file, rank})
       %{"type" => "rook"} ->
         Rook.moves(board, {file, rank})
+      %{"type" => "bishop"} ->
+        []
+      %{"type" => "knight"} ->
+        []
+      %{"type" => "king"} ->
+        []
+      %{"type" => "queen"} ->
+        []
     end
   end
 end
