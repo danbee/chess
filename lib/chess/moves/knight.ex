682ce6765257426c79df4d8fcc2660f9036a0e6e
Dan Barber <github@danbarber.me>
2018-03-13 22:30:39 -0400

    These should be function calls

--- lib/chess/moves/knight.ex
+++ lib/chess/moves/knight.ex
@@ -2,7 +2,7 @@ defmodule Chess.Moves.Knight do
   @moduledoc false
 
   def moves(_board, {file, rank}) do
-    patterns
+    patterns()
     |> Enum.map(fn ({fv, rv}) -> {file + fv, rank + rv} end)
     |> Enum.reject(fn ({file, rank}) ->
       file < 0 || rank < 0 || file > 7 || rank > 7
