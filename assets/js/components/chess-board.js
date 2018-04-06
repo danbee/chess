2e319f762a5ebf3909c6cfac34fa293808d0bbc4
Dan Barber <github@danbarber.me>
2018-04-06 11:25:18 -0400

    Add board labels

--- assets/js/components/chess-board.js
+++ assets/js/components/chess-board.js
@@ -25,40 +25,6 @@ class ChessBoard extends React.Component {
     return store.getState().player;
   }
 
-  renderSquares() {
-    const board = this.getBoard();
-    const { store, channel } = this.props;
-
-    return _.map(this.ranks(), (rankId) => {
-      const rank = this.getBoard()[rankId];
-
-      return _.map(this.files(rank), (fileId) => {
-        return (
-          <ChessBoardSquare
-            file={fileId}
-            key={fileId}
-            rank={rankId}
-            piece={board[rankId][fileId]}
-            store={store}
-            channel={channel}
-          />
-        );
-      });
-    });
-  }
-
-  renderRanks() {
-    const board = this.getBoard();
-
-    return _.map(this.ranks(), rankId => {
-      return (
-        <div className="board-rank" key={rankId}>
-          {this.renderFiles(rankId)}
-        </div>
-      );
-    });
-  }
-
   files(rank) {
     const player = this.getPlayer();
 
@@ -84,6 +50,28 @@ class ChessBoard extends React.Component {
     }
   }
 
+  renderSquares() {
+    const board = this.getBoard();
+    const { store, channel } = this.props;
+
+    return _.map(this.ranks(), (rankId) => {
+      const rank = this.getBoard()[rankId];
+
+      return _.map(this.files(rank), (fileId) => {
+        return (
+          <ChessBoardSquare
+            file={fileId}
+            key={fileId}
+            rank={rankId}
+            piece={board[rankId][fileId]}
+            store={store}
+            channel={channel}
+          />
+        );
+      });
+    });
+  }
+
   get boardClass() {
     const turn = this.getTurn();
     const player = this.getPlayer();
@@ -91,9 +79,40 @@ class ChessBoard extends React.Component {
     return classNames("board", turn + "-to-move", "player-is-" + player);
   }
 
+  get rankLabels() {
+    return [1, 2, 3, 4, 5, 6, 7, 8];
+  }
+
+  get fileLabels() {
+    return ["a", "b", "c", "d", "e", "f", "g", "h"];
+  }
+
+  renderRankLabels() {
+    return _.map(this.rankLabels, (rankLabel) => {
+      return (
+        <div key={rankLabel} className="board-label">{rankLabel}</div>
+      );
+    });
+  }
+
+  renderFileLabels() {
+    return _.map(this.fileLabels, (fileLabel) => {
+      return (
+        <div key={fileLabel} className="board-label">{fileLabel}</div>
+      );
+    });
+  }
+
   render() {
     return (
       <div className={this.boardClass}>
+        <div className="board-rank-labels">
+          {this.renderRankLabels()}
+        </div>
+        <div className="board-file-labels">
+          {this.renderFileLabels()}
+        </div>
+
         <div className="board-body">
           {this.renderSquares()}
         </div>
