d6c0ec5f1389f1caf6e8fc1666a2a3b79f1b0080
Dan Barber <github@danbarber.me>
2018-04-08 22:46:07 -0400

    Welcome page i18n

--- lib/chess_web/views/page_view.ex
+++ lib/chess_web/views/page_view.ex
@@ -1,3 +1,15 @@
 defmodule ChessWeb.PageView do
   use ChessWeb, :view
+
+  def login_link(conn) do
+    gettext("Log in")
+    |> link(to: session_path(conn, :new))
+    |> safe_to_string
+  end
+
+  def register_link(conn) do
+    gettext("Register")
+    |> link(to: registration_path(conn, :new))
+    |> safe_to_string
+  end
 end
