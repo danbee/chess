fc5b500d2721594f913f83da0aee1b1b686a4823
Dan Barber <github@danbarber.me>
2018-05-08 10:56:24 +0200

    Update moves migration

--- priv/repo/migrations/20180504172433_create_moves.exs
+++ priv/repo/migrations/20180504172433_create_moves.exs
@@ -3,7 +3,7 @@ defmodule Chess.Repo.Migrations.CreateMoves do
 
   def change do
     create table(:moves) do
-      add :game_id, references(:games)
+      add :game_id, references(:games, on_delete: :delete_all)
       add :from, :map
       add :to, :map
       add :piece, :map
@@ -11,5 +11,7 @@ defmodule Chess.Repo.Migrations.CreateMoves do
 
       timestamps()
     end
+
+    create index(:moves, [:game_id])
   end
 end
