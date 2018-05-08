49bba88f0747a76a1f131e551a482669853af5d0
Dan Barber <github@danbarber.me>
2018-05-08 13:15:09 +0200

    Consistency

--- lib/chess/moves/pieces/king.ex
+++ lib/chess/moves/pieces/king.ex
@@ -4,10 +4,10 @@ defmodule Chess.Moves.Pieces.King do
   alias Chess.Moves.Generator
 
   def moves(board, {file, rank}) do
-    Generator.moves(board, {file, rank}, patterns())
+    Generator.moves(board, {file, rank}, pattern())
   end
 
-  defp patterns do
+  defp pattern do
     [{1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}]
   end
 end
