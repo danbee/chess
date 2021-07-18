ef8d07e77b0594c02025889b33e85219cecec1e7
Dan Barber <github@danbarber.me>
2021-07-17 21:18:37 -0500

    Upgrade Elixir and remove .buildpacks

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chess.Mixfile do
     [
       app: :chess,
       version: "0.2.0",
-      elixir: "~> 1.11.3",
+      elixir: "~> 1.11.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
