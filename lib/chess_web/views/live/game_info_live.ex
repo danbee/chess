87504064e713f807d1db257fff54e62d48d0c5f6
Dan Barber <github@danbarber.me>
2021-07-17 21:01:11 -0500

    WIP: Game info

---
+++ lib/chess_web/views/live/game_info_live.ex
@@ -0,0 +1,21 @@
+defmodule ChessWeb.GameInfoLive do
+  use Phoenix.LiveView
+
+  alias Chess.Store.User
+  alias Chess.Store.Game
+  alias Chess.Repo
+
+  def render(assigns) do
+    Phoenix.View.render(ChessWeb.GameView, "game_info.html", assigns)
+  end
+
+  def mount(_params, %{"game_id" => game_id, "user_id" => user_id}, socket) do
+    user = Repo.get!(User, user_id)
+
+    game =
+      Game.for_user(user)
+      |> Repo.get!(game_id)
+
+    {:ok, assign(socket, game: game, user: user)}
+  end
+end
