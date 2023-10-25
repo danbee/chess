32cf90d70c93c8ea4a25af99bf66451e3a87b103
Dan Barber <github@danbarber.me>
2023-10-25 16:14:23 -0500

    Switch to NPM and remove old React code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,22 @@
 "use strict";
 
-import {Socket} from "phoenix";
-import LiveSocket from "phoenix_live_view";
+import "phoenix_html";
 
-let csrfToken = document
-  .querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket(
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+
+const csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+
+const liveSocket = new LiveSocket(
   "/live",
   Socket,
   {params: {_csrf_token: csrfToken}}
 );
 
-liveSocket.connect()
+liveSocket.connect();
 
-window.liveSocket = liveSocket
+liveSocket.enableDebug();
 
-import "@babel/polyfill";
-import "phoenix_html";
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-
-import Game from "./components/game";
-import OpponentFinder from "./components/opponent-finder";
-import chessBoardReducer from "./reducers/chess-board";
-
-const store = createStore(chessBoardReducer);
-
-const gameContainer = document.getElementById("game");
-
-if (gameContainer != undefined) {
-  const gameId = gameContainer.getAttribute("data-game-id");
-
-  ReactDOM.render(<Game store={store} gameId={gameId} />, gameContainer);
-}
-
-const opponentFinderContainer = document.getElementById("opponent-finder");
-
-if (opponentFinderContainer != undefined) {
-  ReactDOM.render(<OpponentFinder store={store} />, opponentFinderContainer);
-}
+window.liveSocket = liveSocket;
--- assets/js/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = {
-  findOpponent: (queryString) => {
-    return axios.get("/api/opponents", { params: { q: queryString } });
-  },
-};
-
-export default API;
